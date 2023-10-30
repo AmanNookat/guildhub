7f6e762c714fbfe301aefd6be8eb5226d2b2bde1
AmanNookat <138446723+AmanNookat@users.noreply.github.com>
2023-10-30 23:36:28 +0600

    paymentform

--- components/modals/payment-form-modal.tsx
+++ components/modals/payment-form-modal.tsx
@@ -4,6 +4,9 @@ import qs from "query-string";
 import axios from "axios";
 import { useState } from "react";
 
+import Cards from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
+
 import {
   Dialog,
   DialogContent,
@@ -14,100 +17,92 @@ import {
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "@/components/ui/button";
+import { Input } from "../ui/input";
 
 export const PaymentFormModal = () => {
-  const { isOpen, onClose, type, data } = useModal();
+  const { isOpen, onClose, type } = useModal();
 
   const isModalOpen = isOpen && type === "paymentForm";
-  const { apiUrl, query } = data;
 
-  const [isLoading, setIsLoading] = useState(false);
+  const [state, setState] = useState({
+    number: "",
+    expiry: "",
+    cvc: "",
+    name: "",
+    focus: "",
+  });
 
-  const onClick = async () => {
-    try {
-      setIsLoading(true);
-      const url = qs.stringifyUrl({
-        url: apiUrl || "",
-        query,
-      });
+  const handleInputChange = (evt: any) => {
+    const { name, value } = evt.target;
 
-      await axios.delete(url);
-
-      onClose();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
+    let newValue = value;
+    if (name === "number" || name === "cvc") {
+      newValue = value.replace(/\D/g, "");
     }
+
+    if (name === "expiry") {
+      newValue = value.replace(/\D/g, "").slice(0, 4);
+    }
+
+    setState((prev) => ({ ...prev, [name]: newValue }));
+  };
+
+  const handleInputFocus = (evt: any) => {
+    setState((prev) => ({ ...prev, focus: evt.target.name }));
   };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
-        <div className="w-full max-w-lg mx-auto p-8">
-          <div className="bg-white rounded-lg shadow-lg p-6">
-            <h2 className="text-lg font-medium mb-6">Payment Information</h2>
-            <form>
-              <div className="grid grid-cols-2 gap-6">
-                <div className="col-span-2 sm:col-span-1">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">
-                    Card Number
-                  </label>
-                  <input
-                    type="text"
-                    name="card-number"
-                    id="card-number"
-                    placeholder="0000 0000 0000 0000"
-                    className="w-full py-3 px-4 text-white border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"
-                  />
-                </div>
-                <div className="col-span-2 sm:col-span-1">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">
-                    Expiration Date
-                  </label>
-                  <input
-                    type="text"
-                    name="expiration-date"
-                    id="expiration-date"
-                    placeholder="MM / YY"
-                    className="w-full py-3 px-4 text-white border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"
-                  />
-                </div>
-                <div className="col-span-2 sm:col-span-1">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">
-                    CVV
-                  </label>
-                  <input
-                    type="text"
-                    name="cvv"
-                    id="cvv"
-                    placeholder="000"
-                    className="w-full py-3 px-4 text-white border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"
-                  />
-                </div>
-                <div className="col-span-2 sm:col-span-1">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">
-                    Card Holder
-                  </label>
-                  <input
-                    type="text"
-                    name="card-holder"
-                    id="card-holder"
-                    placeholder="Full Name"
-                    className="w-full py-3 px-4 text-white border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500"
-                  />
-                </div>
-              </div>
-              <div className="mt-8">
-                <button
-                  type="submit"
-                  className="w-full bg-green-500 hover:bg-blue-600 text-white font-medium py-3 rounded-lg focus:outline-none"
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
-          </div>
+      <DialogContent className="bg-white text-black p-5 overflow-hidden">
+        <DialogHeader>
+          <DialogTitle className="my-5">Buy GuildHub Premium</DialogTitle>
+          <Cards
+            number={state.number}
+            expiry={state.expiry}
+            cvc={state.cvc}
+            name={state.name}
+          />
+        </DialogHeader>
+
+        <div className="flex flex-col w-full items-center text-white gap-3">
+          <Input
+            type="text"
+            name="number"
+            placeholder="Card number"
+            value={state.number}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            maxLength={16}
+          />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={state.name}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+          />
+          <Input
+            type="text"
+            name="expiry"
+            placeholder="Validity (mm/yy)"
+            value={state.expiry}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            maxLength={4}
+          />
+          <Input
+            type="text"
+            name="cvc"
+            placeholder="CVC"
+            value={state.cvc}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            maxLength={3}
+          />
+          <Button onClick={onClose} variant="outline">
+            Buy
+          </Button>
         </div>
       </DialogContent>
     </Dialog>
