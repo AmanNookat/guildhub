7ee9ffe7e99fc97907d7181c26458648a56d1cf1
AmanNookat <138446723+AmanNookat@users.noreply.github.com>
2023-11-01 02:42:50 +0600

    likes finish

--- components/chat/chat-like.tsx
+++ components/chat/chat-like.tsx
@@ -1,6 +1,8 @@
-import { Heart } from "lucide-react";
+import { Heart, HeartCrack, HeartIcon, HeartOff } from "lucide-react";
 import { ActionTooltip } from "../action-tooltip";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
 
 interface ChatLikeProps {
   profileId: string;
@@ -11,8 +13,6 @@ const likeAPI = "http://localhost:8000/likes";
 
 const toggleLike = async (IdProfile: string, IdMessage: string) => {
   const { data } = await axios.get(likeAPI);
-
-  console.log(data);
 
   const checkLike = data.find((oneLike: any) => {
     return IdMessage == oneLike.messageId;
@@ -28,7 +28,6 @@ const toggleLike = async (IdProfile: string, IdMessage: string) => {
     await axios.post(likeAPI, likeData);
   } else {
     const isLiked = checkLike.likes.includes(IdProfile);
-    console.log(isLiked);
 
     if (isLiked) {
       checkLike.likes = checkLike.likes.filter(
@@ -42,15 +41,80 @@ const toggleLike = async (IdProfile: string, IdMessage: string) => {
 };
 
 const ChatLike = ({ profileId, messageId }: ChatLikeProps) => {
+  const [liked, setLiked] = useState(false);
+  const [changeClick, setChangeClick] = useState(0);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const handleLikeClick = async () => {
+    setLiked(!liked);
+
+    try {
+      await toggleLike(profileId, messageId);
+
+      const { data } = await axios.get(likeAPI);
+      const checkLike = data.find(
+        (oneLike: any) => messageId == oneLike.messageId
+      );
+
+      if (checkLike) {
+        setLikeCount(checkLike.likes.length);
+      }
+    } catch (error) {
+      console.error("Ошибка при выполнении запроса: ", error);
+      setLiked(!liked);
+    }
+  };
+
+  useEffect(() => {
+    const checkLikeState = async () => {
+      try {
+        const { data } = await axios.get(likeAPI);
+
+        const checkLike = data.find(
+          (oneLike: any) => messageId == oneLike.messageId
+        );
+
+        if (checkLike) {
+          setLikeCount(checkLike.likes.length);
+
+          if (checkLike.likes.includes(profileId)) {
+            setLiked(true);
+            setChangeClick(1);
+          }
+        } else {
+          setLiked(false);
+          setChangeClick(2);
+        }
+      } catch (error) {
+        console.error("Ошибка при загрузке начального состояния: ", error);
+      }
+    };
+    checkLikeState();
+  }, [changeClick]);
+
   return (
-    <ActionTooltip label="1">
-      <Heart
-        onClick={() => {
-          toggleLike(profileId, messageId);
-        }}
-        className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
-      />
-    </ActionTooltip>
+    <div className="flex items-center">
+      <div>
+        <ActionTooltip label={`${likeCount}`}>
+          {!liked ? (
+            <>
+              <Heart
+                onClick={handleLikeClick}
+                className="cursor-pointer ml-auto w-4 h-4 text-zinc-500  dark:hover:text-zinc-300 transition"
+              />
+            </>
+          ) : (
+            <>
+              <Heart
+                onClick={handleLikeClick}
+                className="cursor-pointer ml-auto w-4 h-4 text-rose-500  dark:hover:text-rose-700 transition"
+              />
+            </>
+          )}
+        </ActionTooltip>
+      </div>
+      <div>{likeCount}</div>
+    </div>
   );
 };
 
