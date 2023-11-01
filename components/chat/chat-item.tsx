fe9be4c12071081e2accaba6d2aadab24af7b734
AmanNookat <138446723+AmanNookat@users.noreply.github.com>
2023-11-01 10:49:10 +0600

    точно финал лайков

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -238,11 +238,10 @@ export const ChatItem = ({
         </div>
       </div>
 
-      {canDeleteMessage && (
-        <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
-          <ChatLike messageId={id} profileId={profileId} />
-        </div>
-      )}
+      <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+        <ChatLike messageId={id} profileId={profileId} />
+      </div>
+
       {canDeleteMessage && (
         <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-14 bg-white dark:bg-zinc-800 border rounded-sm">
           {canEditMessage && (
